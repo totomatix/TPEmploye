d2e00b100d04ef8fa20c9d426236017599a2b978
WaitInTheBush <theo.ollivier@gmail.com>
2023-11-17 11:45:59 +0100

    Ajoutez des fichiers projet.

---
+++ TPEmploye/Employe.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPEmploye
+{
+    /// <summary>
+    /// Classe représentant un employé
+    /// </summary>
+    public class Employe
+    {
+        // attributs
+        #region Attributs
+        private uint _Matricule;
+        private string _Nom;
+        private string _Prenom;
+        private DateTime _DateNaissance;
+        private DateTime _DateEmbauche;
+        private decimal _Salaire;
+        #endregion
+
+        #region Constructeurs
+        public Employe(uint matricule, string nom, string prenom, DateTime dateNaissance, DateTime dateEmbauche, decimal salaire)
+        {
+            Matricule = matricule;
+            Nom = nom;
+            Prenom = prenom;
+            DateNaissance = dateNaissance;
+            DateEmbauche = dateEmbauche;
+            Salaire = salaire;
+        }
+        #endregion
+
+        #region Propriétés
+        public uint Matricule { get => _Matricule; set => _Matricule = value; }
+        public string Nom { get => _Nom; set => _Nom = value; }
+        public string Prenom { get => _Prenom; set => _Prenom = value; }
+        public DateTime DateNaissance { get => _DateNaissance; set => _DateNaissance = value; }
+        public DateTime DateEmbauche { get => _DateEmbauche; set => _DateEmbauche = value; }
+        public decimal Salaire { get => _Salaire; set => _Salaire = value; }
+        #endregion
+
+        #region Méthodes
+
+        /// <summary>
+        /// Fonction retournant l'âge de la personne
+        /// </summary>
+        /// <returns></returns>
+        public int Age()
+        {
+            int Age = DateTime.Today.Year - this.DateNaissance.Year;
+            if (this.DateNaissance.DayOfYear > DateTime.Today.DayOfYear)
+            {
+                Age--;
+            }
+
+            return Age;
+        }
+
+
+        /// <summary>
+        /// Fonction retournant l'ancienneté de la personne
+        /// </summary>
+        /// <returns></returns>
+        public int Anciennete()
+        {
+            int Anciennete = DateTime.Today.Year - this.DateEmbauche.Year;
+            if (this.DateEmbauche.DayOfYear > DateTime.Today.DayOfYear)
+            {
+                Anciennete--;
+            }
+            return Anciennete;
+        }
+
+        /// <summary>
+        /// Fonction mettant à jour le salaire de l'employé en fonction de son ancienneté
+        /// </summary>
+        public void AugmentationSalaire()
+        {
+            int Anciennete = this.Anciennete();
+            if (Anciennete < 5)
+            {
+                Salaire *= 1.02m;
+            }
+            else if (Anciennete < 10)
+            {
+                Salaire *= 1.05m;
+            }
+            else
+            {
+                Salaire *= 1.1m;
+
+            }
+        }
+
+        /// <summary>
+        /// Procédure affichant les informations de l'employé
+        /// </summary>
+        public void AfficherEmploye()
+        {
+            Console.WriteLine($"- Matricule : {Matricule}\n" +
+                $"- Nom complet : {Nom.ToUpper()} {Prenom[0].ToString().ToUpper()+Prenom.Substring(1).ToLower()}\n" +
+                $"- Age : {Age()}\n" +
+                $"- Ancienneté : {Anciennete()}\n" +
+                $"- Salaire : {Salaire} ");
+        }
+        #endregion
+    }
+}
---
+++ TPEmploye/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using TPEmploye;
+
+// récupération des données nécessaires
+Console.Write("Matricule : ");
+uint Matricule = uint.Parse(Console.ReadLine());
+Console.Write("Nom : ");
+string Nom = Console.ReadLine();
+Console.Write("Prenom : ");
+string Prenom = Console.ReadLine();
+Console.Write("Date de naissance : ");
+DateTime Naissance = DateTime.Parse(Console.ReadLine(),CultureInfo.GetCultureInfo("fr-FR"));
+Console.Write("Date d'embauche : ");
+DateTime Embauche = DateTime.Parse(Console.ReadLine(), CultureInfo.GetCultureInfo("fr-FR"));
+Console.Write("Salaire : ");
+decimal Salaire = decimal.Parse(Console.ReadLine());
+
+// création de l'employé
+Employe EMP1 = new Employe(Matricule, Nom, Prenom, Naissance, Embauche, Salaire);
+
+// affichage de l'employé
+EMP1.AfficherEmploye();
+// augmentation du salaire
+EMP1.AugmentationSalaire();
+// affichage de l'employé
+EMP1.AfficherEmploye();
+
+
